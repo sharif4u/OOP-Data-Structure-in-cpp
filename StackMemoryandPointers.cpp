c4e6bc8aee150798ab2a103ed9c0ea1dd8135686
MD. SHARIF UDDIN <101212925+sharif4u@users.noreply.github.com>
2022-04-10 12:03:37 +0600

    Stack Memory and Pointers

---
+++ StackMemoryandPointers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num = 8;
+    int num2 = 5;
+    cout << "Value : " << num << endl;
+    cout << "Address :" << &num << endl;
+
+    cout << "Value : " << num2 << endl;
+    cout << "Address :" << &num2 << endl;
+    return 0;
+}
